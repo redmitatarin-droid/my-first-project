cabbab6e23e3613c8674eb55f8276388043d2c6d
Russell <redmitatarin@gmail.com>
2026-09-25 14:41:58 +0300

    Добавил функцию возведения в степень

--- hello.py
+++ hello.py
@@ -12,9 +12,15 @@ def divide(a, b):
         return "На ноль делить нельзя!"
     return a / b
 
+def power(a, b):
+    return a ** b
+
+
+
 print("Простой калькулятор")
 print("5 + 3 =", add(5, 3))
 print("10 - 4 =", subtract(10, 4))
 print("6 * 7 =", multiply(6, 7))
 print("20 / 5 =", divide(20, 5))
 print("8 / 0 =", divide(8, 0))
+print("2 в степени 10 =", power(2, 10))
